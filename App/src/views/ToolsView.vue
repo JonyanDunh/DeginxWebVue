79ddbba5df4df3b8d1aec530693b50ee485bb1a3
JONYANDUNH <jonyandunh@outlook.com>
2022-07-15 23:17:50 +0800

    美化了工具页面

--- App/src/views/ToolsView.vue
+++ App/src/views/ToolsView.vue
@@ -27,95 +27,59 @@ export default {
                 ItemName: "大数据",
                 ItemKey: 5
             }]);
-    }
+    },
+        data() {
+        return {
+            ToolsItems: [
+                {
+                    ItemName: "Websocket中转站",
+                    ItemImg: "https://message.biliimg.com/bfs/im/d55898984e3ed5949e3d083e65228b43579683be.png",
+                    ItemDescrible: "提供Websocket打洞服务,支持多方订阅多方消息发送"
+                },
+                {
+                    ItemName: "Covid-19趋势预测",
+                    ItemImg: "https://th.bing.com/th/id/R.814b05d8d6baac36f24c9a8428cdaefc?rik=ENv5akS8shlHpA&riu=http%3a%2f%2fcidoc.mini.icom.museum%2fwp-content%2fuploads%2fsites%2f6%2f2020%2f05%2f200309-D-HN545-003.jpg&ehk=MxhRzMDEBNKgToe9asbuVKWDoQxJhbeJjKhOC27MZ5I%3d&risl=&pid=ImgRaw&r=0",
+                    ItemDescrible: "基于深度学习的疫情预测,模型数据来自国家卫健委"
+                },
+                {
+                    ItemName: "Bilibili透明头像上传",
+                    ItemImg: "https://media.sketchfab.com/models/2dbf7c52ea6a4d6c832897eea98d5467/thumbnails/a9e69fc4a33e41998513848db9feeda8/b5c6e3d229ec41a7b58d34c015aa9e50.jpeg",
+                    ItemDescrible: "懂的都懂,叔叔把动态头像封了,透明/半透明头像总不会不给吧"
+                },{
+                    ItemName: "SSL泛域名证书生成",
+                    ItemImg: "https://codebriefly.com/wp-content/uploads/2019/02/lets-encrypt.jpg",
+                    ItemDescrible: "在线生成Let's Encrypt的SSL泛域名证书,最多可支持48个域名共用一个证书"
+                },
+                {
+                    ItemName: "Youtube视频分享",
+                    ItemImg: "https://ifa2019warsaw.com/wp-content/uploads/2020/08/6.jpg",
+                    ItemDescrible: "一键把Youtube视频分享到QQ空间、微博、哔哩哔哩、朋友圈"
+                },
+            ]
+        }
+    },
 }
 </script>
 
 <template>
 
 
-    <div class="flex flex-wrap justify-start">
-
-        <div class="card w-80  glass shadow-xl m-2">
-            <figure class="px-10 pt-10">
-                <img src="https://placeimg.com/400/225/arch" alt="Shoes" class="rounded-xl" />
+    <div  class="flex flex-wrap justify-start">
+    <div v-for="item in ToolsItems">
+        <div class="card  w-72 bg-base-100 m-2">
+            <figure >
+                <img :src="item.ItemImg" >
             </figure>
-            <div class="card-body items-center text-center">
-                <h2 class="card-title">钓鱼链接生成器</h2>
-                <p>钓骗子专用，提供精准ip定位</p>
-                <div class="card-actions">
-                    <button class="btn btn-primary">免费试用</button>
-                </div>
-            </div>
-        </div>
-
-        <div class="card w-80 glass m-2">
-            <figure><img src="https://message.biliimg.com/bfs/im/94532f85253e3af31ac677b8e8fe89bf88d3749a.jpg"
-                    alt="car!"></figure>
             <div class="card-body">
-                <h2 class="card-title">Websocket中转站</h2>
-                <p>提供websocket打洞服务</p>
+                <h2 class="card-title">{{ item.ItemName }}</h2>
+                <p style="overflow:scroll" class="h-20">{{ item.ItemDescrible }}</p>
                 <div class="card-actions justify-end">
-                    <button class="btn btn-primary">免费试用</button>
+                    <button class="btn btn-primary">立即使用</button>
                 </div>
             </div>
         </div>
 
-        <div class="card w-80 bg-base-100 shadow-xl image-full m-2">
-            <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
-            <div class="card-body">
-                <h2 class="card-title">哔哩哔哩透明头像上传</h2>
-                <p>干翻陈睿</p>
-                <div class="card-actions justify-end">
-                    <button class="btn btn-primary">免费试用</button>
-                </div>
-            </div>
-        </div>
-
-
-        <div class="card lg:card-side bg-base-100 shadow-xl m-2">
-            <figure><img class="h-full" src="https://placeimg.com/400/400/arch" alt="Album"></figure>
-            <div class="card-body">
-                <h2 class="card-title">Covid-19趋势预测</h2>
-                <p>基于深度学习的疫情数据预测</p>
-                <div class="card-actions justify-end">
-                    <button class="btn btn-primary">免费试用</button>
-                </div>
-            </div>
-        </div>
-
-
-        <div class="card card-side bg-base-100 shadow-xl m-2">
-            <figure><img class="h-full" src="https://placeimg.com/200/280/arch" alt="Movie"></figure>
-            <div class="card-body">
-                <h2 class="card-title">SSL泛域名证书生成</h2>
-                <p>生成Let's Encrypt SSL证书，最多支持48个域名共用</p>
-                <div class="card-actions justify-end">
-                    <button class="btn btn-primary">免费试用</button>
-                </div>
-            </div>
-        </div>
-
-
-        <div class="card w-80 bg-base-100 shadow-xl m-2">
-            <div class="card-body">
-                <h2 class="card-title">QQ群机器人接入</h2>
-                <p>基于Mirai的QQ机器人,最多可添加进10个群</p>
-            </div>
-            <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
-        </div>
-
-        <div class="card w-80 bg-primary text-primary-content m-2">
-            <div class="card-body">
-                <h2 class="card-title">AI换脸</h2>
-                <p>基于Tensorflow+Opencv的人脸替换</p>
-                <div class="card-actions justify-end">
-                    <button class="btn">免费试用</button>
-                </div>
-            </div>
-        </div>
-
-
+    </div>
 
     </div>
 
